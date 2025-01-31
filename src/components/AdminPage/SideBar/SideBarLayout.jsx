96f9f8f4e1dbef59484d71a8790f1c0f7ff1c7ab
Sabeshragav <sabeshragav289@gmail.com>
2025-01-31 14:07:47 +0530

    merged

--- src/components/AdminPage/SideBar/SideBarLayout.jsx
+++ src/components/AdminPage/SideBar/SideBarLayout.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 
 function SideBarLayout({ isSidebarOpen, toggleSidebar }) {
   useEffect(() => {
