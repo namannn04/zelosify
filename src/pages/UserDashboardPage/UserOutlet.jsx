e6c5b914100bc7b819875b74df1e03f7f783186a
Sabeshragav <sabeshragav289@gmail.com>
2025-02-01 15:07:03 +0530

    Added warning overlay

--- src/pages/UserDashboardPage/UserOutlet.jsx
+++ src/pages/UserDashboardPage/UserOutlet.jsx
@@ -21,21 +21,37 @@ export default function UserOutlet() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
-      <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-      <div className="flex flex-1 overflow-hidden">
-        <SideBarLayout
-          isSidebarOpen={isSidebarOpen}
-          toggleSidebar={toggleSidebar}
-        />
-        <main
-          className={`flex-1 flex-center w-full px-2 ${
-            isSidebarOpen ? "ml-64 lg:ml-64" : "ml-20 lg:ml-20"
-          }`}
-        >
-          <Outlet />
-        </main>
+    <>
+      {/* Overlay blurred warning */}
+      <div className="fixed h-screen w-full inset-0 lg:hidden flex items-center justify-center bg-black/70 backdrop-blur-md z-50">
+        <div className="text-center text-white">
+          <h2 className="text-2xl font-semibold">Site Under Development</h2>
+          <p className="mt-2 text-lg opacity-80">
+            Our site is currently optimized for larger screens.
+          </p>
+          <p className="text-lg opacity-80">
+            Please visit on a desktop for the best experience.
+          </p>
+        </div>
       </div>
-    </div>
+
+      {/* main content */}
+      <div className="hidden lg:flex flex-col min-h-screen bg-gray-50">
+        <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+        <div className="flex flex-1 overflow-hidden">
+          <SideBarLayout
+            isSidebarOpen={isSidebarOpen}
+            toggleSidebar={toggleSidebar}
+          />
+          <main
+            className={`flex-1 flex-center w-full px-2 ${
+              isSidebarOpen ? "ml-64 lg:ml-64" : "ml-20 lg:ml-20"
+            }`}
+          >
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </>
   );
 }
