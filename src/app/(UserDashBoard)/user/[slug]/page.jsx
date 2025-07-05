83d55317df7ab71ba743e2efd2b8b315482c9b26
Sabeshragav <sabeshragav289@gmail.com>
2025-07-05 12:22:44 +0530

    RBAC - FRONTEND - TRY1

--- src/app/(UserDashBoard)/user/[slug]/page.jsx
+++ src/app/(UserDashBoard)/user/[slug]/page.jsx
@@ -9,6 +9,9 @@ import SupportPage from "@/pages/UserDashboardPage/Support/SupportPage";
 import TrackingPage from "@/pages/UserDashboardPage/Tracking/TrackingPage";
 import FinancePage from "@/pages/UserDashboardPage/Finance/FinancePage";
 import VendorResourcePage from "@/pages/UserDashboardPage/Vendor/VendorResourcePage";
+import DigitalInitiativePage from "@/pages/UserDashboardPage/DigitalInitiative/DigitalInitiativePage";
+import DummyLayout1 from "@/components/UserDashboardPage/Dummy1/DummyLayout1";
+import DummyLayout2 from "@/components/UserDashboardPage/Dummy2/DummyLayout2";
 
 // Dynamically import the ChatPage with SSR disabled
 const ChatPage = dynamic(
@@ -31,6 +34,9 @@ export default function UserSubPage({ params }) {
       "support",
       "finance",
       "resource",
+      "digital-initiative",
+      "dummy-page-1",
+      "dummy-page-2",
     ].includes(slug)
   ) {
     notFound();
@@ -38,6 +44,7 @@ export default function UserSubPage({ params }) {
 
   // Conditionally render the correct component:
   switch (slug) {
+    // For VENDOR_MANAGER role
     case "messages":
       return <ChatPage />;
     case "payments":
@@ -52,6 +59,16 @@ export default function UserSubPage({ params }) {
       return <FinancePage />;
     case "resource":
       return <VendorResourcePage />;
+
+    // For BUSINESS_STAKEHOLDER role
+    case "digital-initiative":
+      return <DigitalInitiativePage />;
+    case "dummy-page-1":
+      return <DummyLayout1 />;
+    case "dummy-page-2":
+      return <DummyLayout2 />;
+
+    // Default case
     default:
       return null;
   }
---
+++ src/components/UserDashboardPage/Dummy2/DummyLayout2.jsx
@@ -0,0 +1,12 @@
+// This is a dummy layout component for testing purposes
+
+export default function DummyLayout2() {
+  return (
+    <>
+      <div>This is a dummy layout 2 component for testing purposes</div>
+      <div>
+        Follow standards and directory structure for further development
+      </div>
+    </>
+  );
+}
