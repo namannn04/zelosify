77ef79694d4373a2aea4f44a71bc0f3af94166c9
Sabeshragav <sabeshragav289@gmail.com>
2025-07-14 23:13:35 +0530

    refactor: remove hover styles for ghost button variant in button component

--- src/components/UI/shadcn/button.jsx
+++ src/components/UI/shadcn/button.jsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-border bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
